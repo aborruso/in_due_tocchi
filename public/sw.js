f85fb75992d87ff97887213754a80b8f9f3072eb
aborruso <aborruso@gmail.com>
2025-11-09 09:12:44 +0100

    Fix template descriptions and footer component

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Dynamic cache name based on timestamp - changes with each deployment
-const CACHE_VERSION = '20251109-0808';  // Update this with each deployment
+const CACHE_VERSION = '20251109-0812';  // Update this with each deployment
 const CACHE_NAME = `riformula-v${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/in_due_tocchi/',
